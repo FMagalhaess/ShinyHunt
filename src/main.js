dda482c817f92d0e19a764a5eb4ef1bf17357f98
¨Filipe <7filipe093@gmail.com>
2023-05-23 02:51:59 -0300

    Salvando tudo

--- src/main.js
+++ src/main.js
@@ -9,7 +9,11 @@ const phaseCounter = document.querySelector('#phases')
 const screenCounter = document.querySelector('#screens');
 const encountersCounter = document.querySelector('#encounters');
 const resetButton = document.querySelector('#reset-button')
+const pokeImage = document.querySelector('#pokemao')
+const searchBar = document.querySelector('#search-bar')
+const searchPkmn = document.querySelector('#pkmn-search')
 
+let showedPokemon;
 let encounter = 0;
 let phase = 0;
 let screensNumber = 1;
@@ -17,6 +21,7 @@ const increaseScreen = () => {
     screensNumber = screensNumber + 1
     screenCounter.innerHTML = `Telas: ${screensNumber}`
     console.log(screensNumber);
+    localStorage.setItem('screensNumber', screensNumber)
 };
 const decreaseScreen = () => {
   screensNumber = screensNumber - 1;
@@ -27,6 +32,8 @@ const increasePhase = () => {
     phase = phase + 1
     phaseCounter.innerHTML = `Fase: ${phase}`
     console.log(phase);
+    localStorage.setItem("phase", phase);
+
 };
 const decreasePhase = () => {
   phase = phase - 1;
@@ -37,6 +44,8 @@ const increaseEncounter = () => {
     encounter = encounter + screensNumber
     encountersCounter.innerHTML = `Encontros: ${encounter}`;
     console.log(encounter);
+    localStorage.setItem("encounter", encounter);
+
 };
 const decreaseEncounter = () => {
   encounter = encounter - screensNumber;
@@ -47,6 +56,12 @@ const resetEncounter = () => {
     encountersCounter.innerHTML = `Encontros: 0`;
     encounter = 0
 }
+const searchPokemon = () => {
+pokeImage.src = `https://pokemon.night.coffee/icons/shiny/${searchBar.value}.gif`;
+showedPokemon = `https://pokemon.night.coffee/icons/shiny/${searchBar.value}.gif`;
+localStorage.setItem("showedPokemon", showedPokemon);
+}
+
 phaseIncreaseButton.addEventListener("click", increasePhase);
 screenIncreaseButton.addEventListener("click", increaseScreen);
 encounterIncreaseButton.addEventListener("click", increaseEncounter);
@@ -54,3 +69,4 @@ phaseDecreaseButton.addEventListener("click", decreasePhase);
 screenDecreaseButton.addEventListener("click", decreaseScreen);
 encounterDecreaseButton.addEventListener("click", decreaseEncounter);
 resetButton.addEventListener("click", resetEncounter);
+searchPkmn.addEventListener('click', searchPokemon)
